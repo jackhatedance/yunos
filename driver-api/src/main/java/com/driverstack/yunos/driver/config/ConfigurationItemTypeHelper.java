86691424543f28579ba5754d0d3d75758979413d
jack <jackhatedance@gmail.com>
2014-11-11 13:16:44 +0800

    remove(fix) jdk1.8 usage

--- driver-api/src/main/java/com/driverstack/yunos/driver/config/ConfigurationItemTypeHelper.java
+++ driver-api/src/main/java/com/driverstack/yunos/driver/config/ConfigurationItemTypeHelper.java
@@ -32,6 +32,6 @@ public class ConfigurationItemTypeHelper {
 			return new ConfigurationItemType(
 					ConfigurationItemPrimaryType.DEVICE, className);
 		} else
-			throw new RuntimeException("unknown type:" + clazz.getTypeName());
+			throw new RuntimeException("unknown type:" + clazz.getSimpleName());
 	}
 }
