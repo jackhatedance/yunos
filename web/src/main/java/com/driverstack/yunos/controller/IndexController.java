c5bfe7059dd20550e04d9ea9b1e6d1e61c212555
jack <jackhatedance@gmail.com>
2014-12-30 15:16:31 +0800

    add version to index page

--- web/src/main/java/com/driverstack/yunos/controller/IndexController.java
+++ web/src/main/java/com/driverstack/yunos/controller/IndexController.java
@@ -1,20 +1,39 @@
 package com.driverstack.yunos.controller;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class IndexController extends BaseController{
+public class IndexController extends BaseController {
+	@Autowired
+	ServletContext servletContext;
+
 	@RequestMapping("/")
 	public String hello(Model model) {
 		setHeaderModel(model);
-		
-		
+
+		model.addAttribute("applicationVersion", getApplicationVersion());
+
 		return "index";
+	}
+
+	private String getApplicationVersion() {
+		String name = "/META-INF/MANIFEST.MF";
+		Properties props = new Properties();
+		try {
+			props.load(servletContext.getResourceAsStream(name));
+			return (String) props.get("Implementation-Version");
+		} catch (IOException e) {
+
+			throw new RuntimeException(e);
+		}
+
 	}
 }
